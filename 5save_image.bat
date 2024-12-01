53caf49861a384ab44d6d1ca9450e874eb6e6ac3
Y_ENDOU <gfe00726@yahoo.co.jp>
2024-12-01 11:44:47 +0900

    ライブラリのバージョンを明確にした

---
+++ 5save_image.bat
@@ -0,0 +1,3 @@
+docker save ganbo-fastapi -o ganbo-fast.tar
+docker save ganbo-mariadb -o ganbo-maria.tar
+docker save ganbo-phpmyadmin -o ganbo-admin.tar
---
+++ 6load_image.bat
@@ -0,0 +1,3 @@
+docker load -i ganbo-fast.tar
+docker load -i ganbo-maria.tar
+docker load -i ganbo-admin.tar
